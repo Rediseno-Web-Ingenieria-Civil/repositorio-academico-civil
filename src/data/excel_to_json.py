dc4ce08b4dfc34bd7efff5eb261594450d07a8fc
Tomás Cárdenas <tomas.cardenas@ug.uchile.cl>
2024-10-17 11:21:51 -0300

    refactor: change Datatables implementation to vanilla javascript

--- src/data/excel_to_json.py
+++ src/data/excel_to_json.py
@@ -47,11 +47,12 @@ mes_to_int = {
     "Diciembre": 12,
 }
 
-mes_inicial = "Enero" # @param ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
-ano_inicial = 2022 # @param {"type":"slider","min":2020,"max":2040,"step":1}
+mes_inicial = "Enero"  # @param ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
+ano_inicial = 2022  # @param {"type":"slider","min":2020,"max":2040,"step":1}
 
-mes_final = "Septiembre" # @param ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
-ano_final = 2024 # @param {"type":"slider","min":2020,"max":2040,"step":1}
+mes_final = "Septiembre"  # @param ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
+ano_final = 2024  # @param {"type":"slider","min":2020,"max":2040,"step":1}
+
 
 def check_sheet_name(sheet_name: str) -> bool:
     if sheet_name in allowed_sheets:
@@ -86,15 +87,30 @@ if names_failed:
     print("Error: Al menos una hoja no cumple con el formato de nombre 'Mes año'")
     exit()
 
+areas = set()
+rhma = "Recursos Hídricos y Medio Ambiente"
+ecg = "Estructuras y Geotecnia"
+tran = "Transporte"
+area_map = {
+    "Recursos Hídricos y Medio Ambiente": rhma,
+    "Recursos hídricos y medio ambiente": rhma,
+    "RHMA": rhma,
+    "Recursos Hídricos y Medio ambiente": rhma,
+    "ECG": ecg,
+    "Estructura y Geotécnia": ecg,
+    "Estructuras y Geotecnia": ecg,
+    "Estructura y Geotecnia": ecg,
+    "Transporte": tran,
+}
 repositorio_data = {}
 # for ano in range(ano_inicial, ano_final+1):
-for ano in range(ano_final, ano_inicial-1, -1):
+for ano in range(ano_final, ano_inicial - 1, -1):
     ano_data = {}
     # for mes in meses:
     for mes in meses[::-1]:
-        if ano == ano_inicial and mes not in meses[meses.index(mes_inicial):]:
+        if ano == ano_inicial and mes not in meses[meses.index(mes_inicial) :]:
             continue
-        if ano == ano_final and mes not in meses[:meses.index(mes_final)+1]:
+        if ano == ano_final and mes not in meses[: meses.index(mes_final) + 1]:
             continue
         sheet_name = f"{mes} {ano}"
         if not sheet_name in wb.sheetnames:
@@ -126,7 +142,7 @@ for ano in range(ano_final, ano_inicial-1, -1):
             # if row[2]: print(row[2], sheet.cell(row=row_idx, column=2).fill.__dict__.items(), "\n")
             # if sheet.cell(row=row_idx, column=2).fill.start_color.index != "00000000":
             #     autores_dic.add(row[2])
-        
+
         # Convert to DataFrame
         df = pd.DataFrame(mes_data).dropna(axis=0, how="all").dropna(axis=1, how="all")
 
@@ -157,7 +173,9 @@ for ano in range(ano_final, ano_inicial-1, -1):
         df["NOMBRE DE LA PUBLICACIÓN"] = df["NOMBRE DE LA PUBLICACIÓN"].ffill()
 
         # Group by "NOMBRE DE LA PUBLICACIÓN" and combine other columns as lists
-        grouped_df = df.groupby("NOMBRE DE LA PUBLICACIÓN").agg(lambda x: x.dropna().tolist())
+        grouped_df = df.groupby("NOMBRE DE LA PUBLICACIÓN").agg(
+            lambda x: x.dropna().tolist()
+        )
 
         # if element is a list with one element, extract the element
         grouped_df = grouped_df.map(lambda x: x[0] if len(x) == 1 else x)
@@ -169,7 +187,9 @@ for ano in range(ano_final, ano_inicial-1, -1):
         grouped_df["LINK PUBLICACION"] = grouped_df["NOMBRE DE LA PUBLICACIÓN"].map(
             links_publicacion
         )
-        grouped_df["LINK PUBLICADO EN"] = grouped_df["PUBLICADO EN"].map(links_publicado_en)
+        grouped_df["LINK PUBLICADO EN"] = grouped_df["PUBLICADO EN"].map(
+            links_publicado_en
+        )
         grouped_df["LINK DOI"] = grouped_df["DOI"].map(links_doi)
 
         # Add columns for year and month
@@ -178,14 +198,31 @@ for ano in range(ano_final, ano_inicial-1, -1):
 
         # Drop column FECHA
         # grouped_df.drop(columns=["FECHA"], inplace=True)
-        grouped_df["FECHA"] = f'{ano}-{mes_to_int[mes]:02d}-01'
+        grouped_df["FECHA"] = f"{ano}-{mes_to_int[mes]:02d}-01"
+
+        # Map areas
+        grouped_df["AREA"] = grouped_df["AREA"].map(
+            lambda x: area_map[x] if not isinstance(x, list) and x in area_map else x
+        )
 
         # Append to ano_data
         ano_data[f"{mes}"] = grouped_df.to_dict(orient="records")
-    
+
+        area_df = pd.DataFrame()
+        area_df["AREA"] = grouped_df["AREA"].map(
+            lambda x: tuple(x) if isinstance(x, list) else x
+        )
+        # Add areas to set
+        areas.update(area_df["AREA"].unique())
+
     # Append to repositorio_data
     # + " " to avoid javascript sorting the keys
-    repositorio_data[f"{ano}"+" "] = ano_data
+    repositorio_data[f"{ano}" + " "] = ano_data
 
+print("Áreas encontradas:", areas)
+# Save areas to a JSON file
+areas_data = list(areas)
+with open("src/data/areas.json", "w") as f:
+    json.dump(areas_data, f, ensure_ascii=False, indent=4)
 with open("src/data/repositorio_academico.json", "w") as f:
     json.dump(repositorio_data, f, ensure_ascii=False, indent=4)
